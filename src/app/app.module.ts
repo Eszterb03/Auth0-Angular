c861ac3ebe3153c7223354cd8d48c6c6f8a8580b
Eszterb03 <eszter.b03@gmail.com>
2019-02-25 15:06:18 +0100

    add(feat) the basci of the AUth0"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { routing, appRoutingProviders} from './router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { LoginComponent } from './login/login.component';
+
+import {AuthService} from './services/auth.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    ToolbarComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
-  providers: [],
+  providers: [
+    appRoutingProviders,
+    AuthService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/router.ts
@@ -0,0 +1,26 @@
+import {NgModule, ModuleWithProviders} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+
+import {HomeComponent} from './home/home.component';
+import {ProfileComponent} from './profile/profile.component';
+
+const appRoutes: Routes = [
+{
+  path: '',
+  component: HomeComponent
+},
+{
+path: 'profile',
+component: ProfileComponent
+},
+];
+
+export const appRoutingProviders: any [] = [];
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
